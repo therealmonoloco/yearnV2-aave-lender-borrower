ec88f1ca79e94a3c86d5dd5056e04b0086749335
therealmonoloco <therealmonoloco@gmail.com>
2021-09-10 09:18:16 -0400

    test: tend trigger skeleton

---
+++ tests/test_tend_trigger.py
@@ -0,0 +1,29 @@
+from brownie import chain, reverts, Wei
+
+
+def test_higher_ltv_should_take_more_debt():
+    pass
+
+
+def test_higher_ltv_in_rebalancing_band_should_not_take_more_debt():
+    pass
+
+
+def test_lower_ltv_should_repay_debt():
+    pass
+
+
+def test_lower_ltv_inside_rebalancing_band_should_not_repay_debt():
+    pass
+
+
+def test_ltv_calculation_on_withdraw():
+    pass
+
+
+def test_tend_trigger_conditions():
+    pass
+
+
+def test_ltv_over_liquidation_should_revert():
+    pass
